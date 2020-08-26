7b2fcf3f18703e3709e9f9ff788d1d6d6a5cedf8
Dineshchand <dineshchandgr@gmail.com>
2020-08-26 14:44:16 +0800

    Update DeleteDuplicateNode.java

--- src/io/java/DeleteDuplicateNode.java
+++ src/io/java/DeleteDuplicateNode.java
@@ -16,16 +16,17 @@ class DeleteDuplicateNode
 	
     public Node removeDuplicates(Node head) {
     	Node start = head;
-    	Node current = head;
-    	while(current.next != null) {
-    		current = start.next;
-    		
+    	Node current = head.next;
+    	while(current != null) {
     		if(start.data == current.data) {
     			start.next = current.next;
     			current = null;
+    			current = start.next;
+    		}else{
+    			start = start.next;
     		}
     	}
-		return head;
+	return head;
     }
 
 	 public Node insert(Node head,int data)
